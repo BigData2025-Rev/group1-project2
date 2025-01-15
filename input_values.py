293cce9a2a99a36db4bfe1729023075787973074
ortava <RyanTommyTavares@gmail.com>
2025-01-15 11:35:45 -0800

    Fix comma formatting in cities tuples

--- input_values.py
+++ input_values.py
@@ -2,20 +2,20 @@ CITIES_BY_COUNTRY = {
     "US": 
     (
         "New York", "Los Angeles", "Chicago", "Houston", "Peoenix",
-		"Philadelphia", "San Antonio", "San Diego", "Dallas", "Jacksonville"
-		"Austin", "Fort Worth", "San Jose", "Columbus", "Charlotte"
+		"Philadelphia", "San Antonio", "San Diego", "Dallas", "Jacksonville",
+		"Austin", "Fort Worth", "San Jose", "Columbus", "Charlotte",
 		"Indianapolis", "San Francisco", "Seattle", "Denver", "Oklahoma City"
     ),
     "JP": 
     (
-        "Tokyo", "Yokohama", "Osaka", "Nagoya", "Sapporo"
-		"Fukuoka", "Kobe", "Kawasaki", "Kyoto", "Saitama"
+        "Tokyo", "Yokohama", "Osaka", "Nagoya", "Sapporo",
+		"Fukuoka", "Kobe", "Kawasaki", "Kyoto", "Saitama",
 		"Hiroshima", "Sendai", "Chiba", "Kitakyushu", "Sakai"
     ),
     "DE": 
     (
         "Berlin", "Hamburg", "Munich", "Cologne", "Frankfurt am Main",
-		"Stuttgart", "Düsseldorf", "Leipzig", "Dortmund", "Essen"
+		"Stuttgart", "Düsseldorf", "Leipzig", "Dortmund", "Essen",
 		"Bremen", "Dresdent", "Hanover", "Nuremburg", "Duisburg"
     ),
     "FR": 
@@ -25,7 +25,7 @@ CITIES_BY_COUNTRY = {
     "IN": 
     (
         "Mumbai", "Delhi", "Bangalore", "Hyderabad", "Ahmedabad",
-		"Chennai", "Kolkata", "Surat", "Pune", "Jaipur"
+		"Chennai", "Kolkata", "Surat", "Pune", "Jaipur",
 		"Lucknow", "Kanpur", "Nagpur", "Indore", "Thane"
     ),
     "UK": 
