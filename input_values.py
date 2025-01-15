8a7d0fba51b5ae965f842b2ff63befdc3978c45e
ortava <RyanTommyTavares@gmail.com>
2025-01-15 11:17:09 -0800

    Add input_values.py

---
+++ input_values.py
@@ -0,0 +1,38 @@
+CITIES_BY_COUNTRY = {
+    "US": 
+    (
+        "New York", "Los Angeles", "Chicago", "Houston", "Peoenix",
+		"Philadelphia", "San Antonio", "San Diego", "Dallas", "Jacksonville"
+		"Austin", "Fort Worth", "San Jose", "Columbus", "Charlotte"
+		"Indianapolis", "San Francisco", "Seattle", "Denver", "Oklahoma City"
+    ),
+    "JP": 
+    (
+        "Tokyo", "Yokohama", "Osaka", "Nagoya", "Sapporo"
+		"Fukuoka", "Kobe", "Kawasaki", "Kyoto", "Saitama"
+		"Hiroshima", "Sendai", "Chiba", "Kitakyushu", "Sakai"
+    ),
+    "DE": 
+    (
+        "Berlin", "Hamburg", "Munich", "Cologne", "Frankfurt am Main",
+		"Stuttgart", "Düsseldorf", "Leipzig", "Dortmund", "Essen"
+		"Bremen", "Dresdent", "Hanover", "Nuremburg", "Duisburg"
+    ),
+    "FR": 
+    (
+        "Paris", "Marseille", "Lyon", "Toulouse", "Nice"
+    ),
+    "IN": 
+    (
+        "Mumbai", "Delhi", "Bangalore", "Hyderabad", "Ahmedabad",
+		"Chennai", "Kolkata", "Surat", "Pune", "Jaipur"
+		"Lucknow", "Kanpur", "Nagpur", "Indore", "Thane"
+    ),
+    "UK": 
+    (
+        "London", "Birmingham", "Manchester", "Liverpool", "Leeds",
+		"Sheffield", "Teesside", "Bristol"
+    )
+}
+
+COUNTRIES = tuple(CITIES_BY_COUNTRY.keys())
