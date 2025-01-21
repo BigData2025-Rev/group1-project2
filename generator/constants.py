938fbdc0b4152d3c79a73cc69c664dd5d0db8c10
Tin Ngo <tin159iop@yahoo.com>
2025-01-21 08:10:06 -0800

    Generator folder

---
+++ generator/constants.py
@@ -0,0 +1,190 @@
+import pandas as pd
+
+PRODUCTS_DF = pd.read_csv('./tin_products.csv')
+PRODUCTS_CATEGORY = {
+    'accessories': 12,
+    'appliances': 24,
+    'toys & baby products': 20,
+    'car & motorbike': 20,
+    'industrial supplies': 8,
+    'sports & fitness': 16 
+}
+
+PAYMENT_TYPES = {
+    "Debit Card": 10,
+    "Credit Card": 35,
+    "Gift Card": 25,
+    "Bank Transfer": 10,
+    "Digital Wallet": 20
+}
+FAILURE_REASON = {
+    "Debit Card": {
+        "Incorrect card details (number, expiry, CVV)": 10, 
+        "Insufficient funds": 20
+    },
+    "Credit Card": {
+        "Incorrect card details (number, expiry, CVV)": 10, 
+        "credit limit exceeded": 50
+    },
+    "Gift Card": {
+        "Insufficient balance": 10,
+        "Expired card": 20
+    },
+    "Bank Transfer": {
+        "Network or bank server downtime": 20,
+        "Wrong bank details": 10
+    },
+    "Digital Wallet": {
+        "Wallet not funded": 10,
+        "Authentication errors": 20
+    }
+}
+
+CITIES_BY_COUNTRY = {
+    "US": 
+    (
+        "New York", "Los Angeles", "Chicago", "Houston", "Peoenix",
+		"Philadelphia", "San Antonio", "San Diego", "Dallas", "Jacksonville",
+		"Austin", "Fort Worth", "San Jose", "Columbus", "Charlotte",
+		"Indianapolis", "San Francisco", "Seattle", "Denver", "Oklahoma City"
+    ),
+    "JP": 
+    (
+        "Tokyo", "Yokohama", "Osaka", "Nagoya", "Sapporo",
+		"Fukuoka", "Kobe", "Kawasaki", "Kyoto", "Saitama",
+		"Hiroshima", "Sendai", "Chiba", "Kitakyushu", "Sakai"
+    ),
+    "DE": 
+    (
+        "Berlin", "Hamburg", "Munich", "Cologne", "Frankfurt am Main",
+		"Stuttgart", "Düsseldorf", "Leipzig", "Dortmund", "Essen",
+		"Bremen", "Dresdent", "Hanover", "Nuremburg", "Duisburg"
+    ),
+    "FR": 
+    (
+        "Paris", "Marseille", "Lyon", "Toulouse", "Nice"
+    ),
+    "IN": 
+    (
+        "Mumbai", "Delhi", "Bangalore", "Hyderabad", "Ahmedabad",
+		"Chennai", "Kolkata", "Surat", "Pune", "Jaipur",
+		"Lucknow", "Kanpur", "Nagpur", "Indore", "Thane"
+    ),
+    "UK": 
+    (
+        "London", "Birmingham", "Manchester", "Liverpool", "Leeds",
+		"Sheffield", "Teesside", "Bristol"
+    )
+}
+
+ECOMMERCE_WEBSITE = {
+    "Amazon": 50,
+    "AliExpress": 10,
+    "eBay": 5,
+    "Temu": 5,
+    "Walmart": 5,
+    "Etsy": 25
+}
+
+MONTH_WEIGHTS_BY_CATEGORY = {
+    'accessories': [0.05, 0.04, 0.05, 0.06, 0.07, 0.10, 0.12, 0.15, 0.13, 0.11, 0.07, 0.05],
+    'appliances': [0.09, 0.06, 0.05, 0.04, 0.02, 0.02, 0.05, 0.10, 0.15, 0.20, 0.12, 0.10],
+    'toys & baby products': [0.05, 0.04, 0.05, 0.06, 0.07, 0.10, 0.12, 0.15, 0.13, 0.11, 0.07, 0.05],
+    'car & motorbike': [0.05, 0.04, 0.05, 0.06, 0.07, 0.10, 0.12, 0.15, 0.13, 0.11, 0.07, 0.05],
+    'industrial supplies': [0.05, 0.04, 0.05, 0.06, 0.07, 0.10, 0.12, 0.15, 0.13, 0.11, 0.07, 0.05],
+    'sports & fitness': [0.05, 0.04, 0.05, 0.06, 0.07, 0.10, 0.12, 0.15, 0.13, 0.11, 0.07, 0.05] 
+}
+
+TIME_WEIGHTS_BY_COUNTRY = {
+    "US": [5, 5, 2, 2, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1],
+    "JP": [1, 1, 1, 1, 5, 5, 5, 10, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    "DE": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 6, 7, 6, 5, 1, 1, 1, 1, 1, 1, 1, 1],
+    "FR": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 2, 2, 5, 10, 4, 1, 1, 1, 1, 1, 1],
+    "IN": [1, 1, 4, 5, 6, 5, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    "UK": [1, 1, 4, 1, 1, 1, 1, 1, 3, 4, 5, 6, 6, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+}
+
+YEARS = {
+    "2021": 5, 
+    "2022": 15,
+    "2023": 30,
+    "2024": 50
+}
+
+COUNTRIES = ["US", "JP", "DE", "FR", "IN", "UK"]
+COUNTRY_WEIGHTS = {
+    "US": 0.40,
+    "JP": 0.15,
+    "DE": 0.10,
+    "FR": 0.05,
+    "IN": 0.20,
+    "UK": 0.10,
+}
+CATEGORY_WEIGHTS = {
+    "appliances": {
+        "weight_by_country":
+            {
+                "US": .15,
+                "JP": .15,
+                "DE": .10,
+                "FR": .10,
+                "IN": .20,
+                "UK": .30
+            }
+    },
+    "toys & baby products": {
+        "weight_by_country":
+            {
+                "US": .20,
+                "JP": .05,
+                "DE": .10,
+                "FR": .25,
+                "IN": .25,
+                "UK": .15
+            }
+    },
+    "accessories": {
+        "weight_by_country":
+            {
+                "US": .15,
+                "JP": .30,
+                "DE": .10,
+                "FR": .20,
+                "IN": .15,
+                "UK": .10
+            }
+    },
+    "industrial supplies": {
+        "weight_by_country":
+            {
+                "US": .30,
+                "JP": .05,
+                "DE": .10,
+                "FR": .10,
+                "IN": .30,
+                "UK": .15
+            }
+    },
+    "sports & fitness": {
+        "weight_by_country":
+            {
+                "US": .10,
+                "JP": .20,
+                "DE": .30,
+                "FR": .15,
+                "IN": .15,
+                "UK": .10
+            }
+    },
+    "car & motorbike": {
+        "weight_by_country":
+            {
+                "US": .30,
+                "JP": .10,
+                "DE": .10,
+                "FR": .25,
+                "IN": .05,
+                "UK": .20
+            }
+    }
+}
--- generator/tin_generator.py
+++ generator/tin_generator.py
@@ -2,8 +2,8 @@ import random
 import string
 from pyspark.sql import SparkSession
 from faker import Faker
-from constants import PRODUCTS_DF, PRODUCTS_CATEGORY, PAYMENT_TYPES, FAILURE_REASON, COUNTRIES, CITIES_BY_COUNTRY, ECOMMERCE_WEBSITE
-
+from constants import PRODUCTS_DF, PRODUCTS_CATEGORY, PAYMENT_TYPES, FAILURE_REASON, COUNTRIES, CITIES_BY_COUNTRY, ECOMMERCE_WEBSITE, TIME_WEIGHTS_BY_COUNTRY, MONTH_WEIGHTS_BY_CATEGORY, YEARS, COUNTRY_WEIGHTS, CATEGORY_WEIGHTS
+from datetime import datetime, date, time, timezone
 
 ROWS = 15000
 SEED = 1234
@@ -28,14 +28,20 @@ def generate_data(order_id):
 
     payment_txn_id = generate_id(12)
     chance = random.randint(1, 10)
-    if chance <= 8:
+    if chance <= 7:
         payment_txn_success = True
         failure_reason = ""
     else:
         payment_txn_success = False
         failure_reason = generate_failure_reason(payment_type)
 
-    country, city = generate_place()
+    country = generate_country(product_category)
+    city = generate_city(country)
+
+    d = generate_date(product_category)
+    t = generate_time(country)
+    timestamp = datetime.combine(d, t, tzinfo=timezone.utc)
+
     return (
         order_id,
         customer_id,
@@ -46,7 +52,7 @@ def generate_data(order_id):
         payment_type,
         qty,
         price,
-        fake.date_time(),
+        timestamp,
         country,
         city,
         ecommerce_website_name,
@@ -85,14 +91,37 @@ def generate_failure_reason(payment_type):
     weights = list(payment_type_reasons.values())
     return random.choices(options, weights=weights)[0]
 
-def generate_place():
-    options = list(COUNTRIES.keys())
-    weights = list(COUNTRIES.values())
-    country = random.choices(options, weights=weights)[0]
+def generate_country(category):
+    return random.choices(COUNTRIES, [COUNTRY_WEIGHTS[country] * CATEGORY_WEIGHTS[category]['weight_by_country'][country] for country in COUNTRY_WEIGHTS], k=1)[0]
 
-    city = random.choice(CITIES_BY_COUNTRY[country])
+def generate_city(country):
+    return random.choice(CITIES_BY_COUNTRY[country])
 
-    return country, city
+def generate_date(category):
+    options = range(1, 13)
+    weights = MONTH_WEIGHTS_BY_CATEGORY[category]
+    month = random.choices(options, weights=weights)[0]
+
+    options = list(YEARS.keys())
+    weights = list(YEARS.values())
+    year = random.choices(options, weights=weights)[0]
+    year = int(year)
+    
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        day = random.randint(1, 31)
+    elif month in [4, 6, 9, 11]:
+        day = random.randint(1, 30)
+    else:
+        day = random.randint(1, 29 if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0) else 28)
+    
+    return date(year, month, day)
+
+def generate_time(country):
+    options = range(24)
+    weights = TIME_WEIGHTS_BY_COUNTRY[country]
+    hour = random.choices(options, weights=weights)[0]
+    second = random.randint(0, 59)
+    return time(hour, second,  tzinfo=timezone.utc)
 
 if __name__ == "__main__":
     columns = ["order_id", "customer_id", "customer_name", "product_id", "product_name", "product_category", "payment_type", "qty", "price", "datetime", "country", "city", "ecommerce_website_name", "payment_txn_id", "payment_txn_success", "failure_reason"]
